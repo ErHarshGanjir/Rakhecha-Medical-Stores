ca640b16bf1fcb5ab7184fe456ede35419cf1ba8
harsh <204142915+ErHarshGanjir@users.noreply.github.com>
2026-08-20 16:49:24 +0530

    Add service worker configuration for RoboPost

---
+++ robopost/app/sw.js
@@ -0,0 +1,20 @@
+{
+  "name": "RpboPost",
+  "short_name": "RoboPost",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#0f172a",
+  "icons": [
+    {
+      "src": "https://i.ibb.co/9kJKdcQ4/text-robo-post-removebg-preview.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://i.ibb.co/9kJKdcQ4/text-robo-post-removebg-preview.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
